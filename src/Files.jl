7161369a1dd2d17ba2b1504350b09b67cf137018
Jeffrey Varner <jdv27@cornell.edu>
2025-06-11 10:43:39 -0400

    Package update
    package update

--- src/Files.jl
+++ src/Files.jl
@@ -1,3 +1,18 @@
+function _puzzleparse(filepath::String)::Vector{String}
+
+    # initialize -
+    result = Vector{String}();
+
+    open(filepath, "r") do io
+        for line ∈ eachline(io)
+            push!(result, strip(line)); # cutoff whitespace
+        end
+    end
+    return result;
+end
+
+
+
 """
     MyKaggleCustomerSpendingDataset() -> DataFrame
 
@@ -6,4 +21,32 @@ The original dataset can be found at: [Spending dataset](https://www.kaggle.com/
 """
 function MyKaggleCustomerSpendingDataset()::DataFrame
     return CSV.read(joinpath(_PATH_TO_DATA, "mall-customers-dataset.csv"), DataFrame)
+end
+
+"""
+    MyStringDecodeChallengeDataset() -> NamedTuple
+
+Load the String Decode Challenge testing and production datasets. 
+
+### Return
+- `NamedTuple`: A tuple containing the three datasets:
+    - `test_part_1`: The first part of the test dataset.
+    - `test_part_2`: The second part of the test dataset.
+    - `production`: The production dataset.
+"""
+function MyStringDecodeChallengeDataset()::NamedTuple
+
+    # load three datasets -
+    test_part_1 = _puzzleparse(joinpath(_PATH_TO_DATA, "test_part_1.txt"));
+    test_part_2 = _puzzleparse(joinpath(_PATH_TO_DATA, "test_part_2.txt"));
+    production_data = _puzzleparse(joinpath(_PATH_TO_DATA, "production.txt"));
+
+    # package into a NamedTuple -
+    data_tuple = (
+        test_part_1 = test_part_1,
+        test_part_2 = test_part_2,
+        production = production_data
+    );
+
+    return data_tuple;
 end
